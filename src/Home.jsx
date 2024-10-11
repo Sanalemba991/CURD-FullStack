f0cd745075c04038fb45e5d3bbcec29cc39c390d
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-11 19:30:38 +0530

    CSS Applying

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import './Home.css'
 function Home() {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
@@ -22,19 +22,23 @@ function Home() {
 
   return (
     <>
-      <div>
-        <h1>User List</h1>
+      <div >
+        <h1 className='head'>User List</h1>
         <div>ADD</div>
         <Link to="/create">Create</Link>
         {error && <p>{error}</p>}
         {data.map(user => (
-          <div key={user.id}>
-            ID: {user.id} | Name: {user.name} | Username: {user.username} | Email: {user.email} | Phone: {user.phone} | Website: {user.website}
-           <Link to={`/read/${user.id}`}>Read</Link>
-            <button>Edit</button>
-            <button>Delete</button>
-         
-          </div>
+      <div key={user.id} className="user-item">
+      <span className="Name">Name: {user.name}</span> | 
+      <span className="username">Username: {user.username}</span> | 
+      <span className="email">Email: {user.email}</span> | 
+      <span className="phone">Phone: {user.phone}</span> | 
+      <span className="website">Website: {user.website}</span>
+      <Link to={`/read/${user.id}`}>Read</Link>
+      <button>Edit</button>
+      <button>Delete</button>
+  </div>
+  
         ))}
       </div>
     </>
