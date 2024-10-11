5ccf0f05398bd31b84abb5471041a342eef42a9d
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-11 18:52:23 +0530

    Adding New Post Submit Button fixed it

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,51 +1,66 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function Create() {
-  const [value,setValue]=useState({
-    name:"",
-    email:'',
-     phoneNo:''
-  })
-  const navigate=useNavigate
-  const submit=(e)=>{
-    event.preventDefault();
-  axios.post('http://localhost:3000/users',value)
-  .then(res=>{console.log(res);
-    navigate('/')
-  })
-  .catch(err=> console.log(err));
+  const [value, setValue] = useState({
+    name: "",
+    email: "",
+    phoneNo: ""
+  });
+
+  const navigate = useNavigate();
+
+  const submit = (e) => {
+    e.preventDefault();
+
+    axios.post('http://localhost:3000/users', value)
+      .then(res => {
+        console.log(res);
+        navigate('/');
+      })
+      .catch(err => console.log(err));
   }
-  
 
-
-  
   return (
-    <>
     <div>
       <h1>Create Me</h1>
-     <form onSubmit={submit}>
-      <div>
-
-      <label htmlFor='name'>Name:</label>
-      <input type='text' name='name' placeholder='Enter Name'
-      onChange={e=>setValue({...value,name:e.target.value})}></input>
-      </div>
-     <div>
-     <label htmlFor='email'>Email:</label>
-     <input type="text" name='email'placeholder='Enter Email'onChange={e=>setValue({...value,email:e.target.value})}></input>
-
-     </div>
-     
-      <label htmlFor='email'>Phone:</label>
-    <input type='text' name='phoneNo' placeholder='enter phone number'onChange={e=>setValue({...value,phoneNo:e.target.value})}></input>
-    <button >Submit</button>
-    <Link to='./'>Back</Link>
-     </form>
-
+      <form onSubmit={submit}>
+        <div>
+          <label htmlFor='name'>Name:</label>
+          <input
+            type='text'
+            name='name'
+            placeholder='Enter Name'
+            onChange={e => setValue({ ...value, name: e.target.value })}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor='email'>Email:</label>
+          <input
+            type='email'
+            name='email'
+            placeholder='Enter Email'
+            onChange={e => setValue({ ...value, email: e.target.value })}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor='phoneNo'>Phone:</label>
+          <input
+            type='tel'
+            name='phoneNo'
+            placeholder='Enter Phone Number'
+            onChange={e => setValue({ ...value, phoneNo: e.target.value })}
+            required
+          />
+        </div>
+        <button type='submit'>Submit</button>
+        <Link to='./'>Back</Link>
+      </form>
     </div>
-    </>
-  )
+  );
 }
 
-export default Create
+export default Create;
