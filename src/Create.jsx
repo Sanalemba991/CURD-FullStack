898689cc268c4100b3b35f1eb31ab484cea78946
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-11 18:34:47 +0530

    Creatong

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react'
 
-const Create = () => {
-  return <h1>Create Page</h1>;
-};
+function Create() {
+  const [value,setValue]=useState({
+    name:"",
+    email:'',
+     phoneNo:''
+  })
+  return (
+    <>
+    <div>
+      <h1>Create Me</h1>
+     <form>
+      <div>
 
-export default Create;
+      <label htmlFor='name'>Name:</label>
+      <input type='text' name='name' placeholder='Enter Name'></input>
+      </div>
+     <div>
+     <label htmlFor='email'>Email:</label>
+     <input type="text" name='email'placeholder='Enter Email'></input>
+
+     </div>
+     
+      <label htmlFor='email'>Phone:</label>
+    <input type='text' name='phoneNo' placeholder='enter phone number'></input>
+    <button >Submit</button>
+     </form>
+
+    </div>
+    </>
+  )
+}
+
+export default Create
