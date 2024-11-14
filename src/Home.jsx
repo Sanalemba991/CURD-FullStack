90029ee96bb187071baf7e8ec4f383d93d4ab7f6
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-14 18:00:24 +0530

    Home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import './Home.css'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
 function Home() {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
@@ -9,10 +9,10 @@ function Home() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:3000/users');
+        const response = await axios.get("http://localhost:3000/users");
         setData(response.data);
       } catch (err) {
-        setError('Failed to fetch users');
+        setError("Failed to fetch users");
         console.error(err);
       }
     };
@@ -22,25 +22,26 @@ function Home() {
 
   return (
     <>
-      <div >
-        <h1 className='head'>User List</h1>
+      <div>
+        <h1 className="head">User List</h1>
         <div>ADD</div>
-        
+
         <Link to="/create">Create</Link>
         {error && <p>{error}</p>}
-        {data.map(user => (
-          <div className='box'>   <div key={user.id} className="user-item">
-          <span className="Name">Name: {user.name}</span> | 
-          <span className="username">Username: {user.username}</span> | 
-          <span className="email">Email: {user.email}</span> | 
-          <span className="phone">Phone: {user.phone}</span> | 
-          <span className="website">Website: {user.website}</span>
-          <Link to={`/read/${user.id}`}>Read</Link>
-          <button>Edit</button>
-          <button>Delete</button>
-      </div></div>
-   
-  
+        {data.map((user) => (
+          <div className="box">
+            {" "}
+            <div key={user.id} className="user-item">
+              <span className="Name">Name: {user.name}</span> |
+              <span className="username">Username: {user.username}</span> |
+              <span className="email">Email: {user.email}</span> |
+              <span className="phone">Phone: {user.phone}</span> |
+              <span className="website">Website: {user.website}</span>
+              <Link to={`/read/${user.id}`}>Read</Link>
+              <button>Edit</button>
+              <button>Delete</button>
+            </div>
+          </div>
         ))}
       </div>
     </>
